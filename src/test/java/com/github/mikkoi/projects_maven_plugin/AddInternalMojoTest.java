463962e0d902856df127229507c96a445cdd4055
Mikko Johannes Koivunalho <mikko.koivunalho@iki.fi>
2025-08-03 10:50:47 +0200

    Add AddInternalMojoTest

---
+++ src/test/java/com/github/mikkoi/projects_maven_plugin/AddInternalMojoTest.java
@@ -0,0 +1,12 @@
+package com.github.mikkoi.projects_maven_plugin;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.project.MavenProject;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class AddInternalMojoTest {
+
+}
